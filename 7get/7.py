7dd15a11bcfac5aca0559547a6f3c67b988ff0ad
ArthurJraghatspanyan <art.arm.2003@gmail.com>
2024-11-14 12:43:11 +0400

    third commit

--- 7get/7.py
+++ 7get/7.py
@@ -1,2 +1,9 @@
 import requests
 
+url = "https://jsonplaceholder.typicode.com/invalid-url"
+
+r = requests.get(url)
+
+if r.status_code == 404:
+    raise FileNotFoundError("Error 404: Not Found!")
+print(f"Status code isL {r.status_code}")
